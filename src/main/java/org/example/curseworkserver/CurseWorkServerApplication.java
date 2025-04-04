4fc9b0d35f9d4c80853275083b530bb33ca77111
Nikita <yakovnikita01@gmail.com>
2025-04-04 11:34:17 +0300

    Create repository

---
+++ src/main/java/org/example/curseworkserver/CurseWorkServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.curseworkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseWorkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseWorkServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/curseworkserver/CurseWorkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.curseworkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseWorkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
